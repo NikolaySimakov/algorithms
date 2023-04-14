d37e6518f0e23eaf286063f991652ec19df0922f
Nikolay Simakov <n.simakoff@gmail.com>
2023-04-15 02:33:31 +0300

    Task: remove element from linked list

---
+++ leetcode/data_structures/linked_list/remove_element.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+
+        dummy = ListNode(val=0, next=head)
+        tail = dummy
+
+        while tail.next:
+            if tail.next.val == val:
+                tail.next = tail.next.next
+            else:
+                tail = tail.next
+
+        return dummy.next
