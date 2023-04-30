d406052201ea39d80121e5f47b24fdb0a1a58e12
Nikolay Simakov <n.simakoff@gmail.com>
2023-04-30 15:21:23 +0300

    Other solutions to the problem

--- codewars/numberFormat.js
+++ codewars/numberFormat.js
@@ -2,9 +2,17 @@
 * Format: (xxx) xxx-xxxx
 */
 
-function createPhoneNumber(numbers){
+function createPhoneNumber1(numbers) {
     const s1 = (numbers.slice(0, 3).map(x => x.toString())).join('');
     const s2 = (numbers.slice(3, 6).map(x => x.toString())).join('');
     const s3 = (numbers.slice(6).map(x => x.toString())).join('');
     return `(${s1}) ${s2}-${s3}`
+}
+
+function createPhoneNumber2(numbers) {
+  return numbers.join('').replace(/(...)(...)(.*)/, '($1) $2-$3');
+}
+
+function createPhoneNumber3(numbers) {
+   return numbers.reduce((p,c) => p.replace('x',c), "(xxx) xxx-xxxx");
 }
