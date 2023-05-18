170cdcd747490efa3eedc0f388434e26ade6171d
Nikolay Simakov <n.simakoff@gmail.com>
2023-05-18 21:33:09 +0300

    Jump game I

---
+++ leetcode/dynamic_programming/55.go
@@ -0,0 +1,25 @@
+package dynamicprogramming
+
+/*
+Solution: we run from n-2 to 0 and for each next value in the jump range
+we check whether it is possible to get there.
+And so on up to position 0.
+*/
+
+func canJump(nums []int) bool {
+
+	n := len(nums)
+	dp := make([]bool, n-1)
+	dp = append(dp, true)
+
+	for i := n - 2; i >= 0; i-- {
+		for j := i; j <= i+nums[i]; j++ {
+			if dp[j] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+
+	return dp[0]
+}
