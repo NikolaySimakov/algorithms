884299fe50a2cdb7837d1cc6ed2c6d9cf6eb07fc
Nikolay Simakov <n.simakoff@gmail.com>
2023-06-01 23:47:36 +0300

    Another solution 1968

--- leetcode/sorting/1968.py
+++ leetcode/sorting/1968.py
@@ -1,7 +1,9 @@
 from typing import List
 
 class Solution:
-    def rearrangeArray(self, nums: List[int]) -> List[int]:
+    
+    # trivial solution
+    def rearrangeArray1(self, nums: List[int]) -> List[int]:
         for i in range(1, len(nums) - 1):
             if nums[i-1] + nums[i+1] == 2 * nums[i]:
                 nums[i+1], nums[i] = nums[i], nums[i+1]
@@ -10,4 +12,13 @@ class Solution:
             if nums[i-1] + nums[i+1] == 2 * nums[i]:
                 nums[i-1], nums[i] = nums[i], nums[i-1]
 
+        return nums
+    
+    # math solution
+    # you know that if a, b < c or a, b > c => (a + b)/2 != c
+    def rearrangeArray2(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums) - 1):
+            if (nums[i-1] < nums[i] < nums[i+1]) or (nums[i-1] > nums[i] > nums[i+1]):
+                nums[i+1], nums[i] = nums[i], nums[i+1]
+        
         return nums
