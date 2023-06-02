f3dce936dd40df6677bb9fad8e98180dcc65d3d1
Nikolay Simakov <n.simakoff@gmail.com>
2023-06-02 22:29:18 +0300

    Sliding window using queue

---
+++ leetcode/data_structures/queue/239.py
@@ -0,0 +1,17 @@
+
+from collections import deque
+from typing import List
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        q = deque()
+        res = []
+        for i in range(len(nums)):
+            while q and nums[q[-1]] < nums[i]:
+                q.pop()
+            q.append(i)
+            if q[0] == i - k:
+                q.popleft()
+            if i >= k - 1:
+                res.append(nums[q[0]])
+        return res
