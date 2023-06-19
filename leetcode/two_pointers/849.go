54f839dfe54376d5eb97314f0453549f3cadcfda
Nikolay Simakov <n.simakoff@gmail.com>
2023-06-19 23:55:39 +0300

    Update leetcode 849

--- leetcode/two_pointers/849.go
+++ leetcode/two_pointers/849.go
@@ -1,3 +1,5 @@
+package twopointers
+
 func maxDistToClosest(seats []int) int {
 	currentIndex, maxDistance := -1, 0
 
@@ -6,21 +8,15 @@ func maxDistToClosest(seats []int) int {
 			continue
 		}
 
-		// handle the case when seats start with zeros 
-		// [0, 0, 0, 1]
 		if currentIndex == -1 {
 			maxDistance = i
 		} else {
-			// handle the general case: between two '1's
-			// [1, 0, 0, 1]
 			maxDistance = max(maxDistance, (i-currentIndex)/2)
 		}
 
 		currentIndex = i
 	}
 
-	// handle the case when seats end with zeros 
-	// [1, 0, 0, 0]
 	return max(maxDistance, len(seats)-currentIndex-1)
 }
 
