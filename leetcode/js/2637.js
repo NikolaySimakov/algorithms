0ba63e2cfa3c8d8d9b8429b5c235d9489b0f1ad4
Nikolay Simakov <n.simakoff@gmail.com>
2023-06-21 17:16:04 +0300

    Promise Time Limit: reject TLE if fn time > t

---
+++ leetcode/js/2637.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+	return async function(...args) {
+        const originPromise = fn(...args);
+        const newPromise = new Promise((_, reject) => {
+            setTimeout(() => reject('Time Limit Exceeded'), t)
+        });
+
+        return Promise.race([
+            originPromise,
+            newPromise,
+        ])
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
