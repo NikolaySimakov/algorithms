ac479df557575da94709555aefaeaf235dc6bf3f
Nikolay Simakov <n.simakoff@gmail.com>
2023-07-09 22:53:12 +0300

    Leetcode 179: update

--- leetcode/sorting/179.py
+++ leetcode/sorting/179.py
@@ -2,6 +2,23 @@ from functools import cmp_to_key
 from typing import List
 
 class Solution:
+    
+    # easy to understand solution
+    def largestNumberSimple(self, nums: List[int]) -> str:
+        nums = list(map(str, nums)) # nums from int to string
+        max_number = ""
+        
+        while len(nums) > 0:
+            curr_max = "0"
+            for num in nums:
+                if num + curr_max > curr_max + num:
+                    curr_max = num
+            max_number += curr_max
+            nums.remove(curr_max)
+            
+        return max_number if max_number[0] != "0" else "0"
+    
+    # using functools.cmp_to_key
     def largestNumber(self, nums: List[int]) -> str:
         nums = list(map(str, nums)) # nums from int to string
         
@@ -9,5 +26,4 @@ class Solution:
             return -1 if a + b > b + a else 1
         
         nums = sorted(nums, key=cmp_to_key(compare)) # sort nums by key compare function
-        res = ''.join(nums)
-        return str(int(res))
+        return str(int(''.join(nums))) # remove leading zeros
