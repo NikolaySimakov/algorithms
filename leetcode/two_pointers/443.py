9866fc240decbf542c664bdef98ae44e0673dc30
Nikolay Simakov <n.simakoff@gmail.com>
2023-07-09 15:04:10 +0300

    Leetcode 443: comments

--- leetcode/two_pointers/443.py
+++ leetcode/two_pointers/443.py
@@ -1,11 +1,20 @@
 from typing import List
 
+'''
+Using Two Pointers to solve task:
+[a, a, a, b, b, c, c, c, c] -> [a, 3, b, 2, c, 4]
+[b, b, b, b, b, b, b, b, b, b, b] -> [b, 1, 1]
+With constant extra space
+
+based on Leetcode 26
+'''
+
 class Solution:
     def compress(self, chars: List[str]) -> int:
         count = 1
         left = 0
 
-        for right in range(1,len(chars)+1):
+        for right in range(1, len(chars) + 1):
             if right < len(chars) and chars[right-1] == chars[right]:
                 count += 1
             else:
