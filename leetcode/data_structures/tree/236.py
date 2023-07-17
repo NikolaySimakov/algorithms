82a0aaabcafa31d7fb7025f797b50235b2480c95
Nikolay Simakov <n.simakoff@gmail.com>
2023-07-17 23:49:27 +0300

    Leetcode 236

---
+++ leetcode/data_structures/tree/236.py
@@ -0,0 +1,23 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if root is None:
+            return None
+        
+        if root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left is None:
+            return right
+        elif right is None:
+            return left
+        else:
+            return root
